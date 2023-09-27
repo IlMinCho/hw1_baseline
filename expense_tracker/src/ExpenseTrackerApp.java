b1d90e9285b54af6394d82a9b16a4c1a19bdcc52
Ilmin Cho <choim426@gmail.com>
2023-09-27 19:20:38 -0400

    All files are pushed

--- expense_tracker/src/ExpenseTrackerApp.java
+++ expense_tracker/src/ExpenseTrackerApp.java
@@ -1,4 +1,5 @@
 import javax.swing.table.DefaultTableModel;
+import javax.swing.JOptionPane;
 
 /**
  * The ExpenseTrackerApp class allows users to add/remove daily transactions.
@@ -34,6 +35,34 @@ public class ExpenseTrackerApp {
       // Call controller to add transaction
       view.addTransaction(t);
     });
+
+    // Handle add transaction button clicks
+    view.getAddTransactionBtn().addActionListener(e -> {
+
+    // Get transaction data from view
+    double amount = view.getAmountField(); 
+    String category = view.getCategoryField();
+
+    // Input validations
+    if (!InputValidation.isValidAmount(amount)) {
+        // Inform user about invalid amount and return
+        JOptionPane.showMessageDialog(null, "Please enter a valid amount greater than 0 and less than 1000!");
+        return;
+    }
+    
+    if (!InputValidation.isValidCategory(category)) {
+        // Inform user about invalid category and return
+        JOptionPane.showMessageDialog(null, "Please enter a valid category food, travel, bills, entertainment, other!");
+        return;
+    }
+
+    // Create transaction object
+    Transaction t = new Transaction(amount, category);
+
+    // Call controller to add transaction
+    view.addTransaction(t);
+    });
+
 
   }
 
---
+++ expense_tracker/src/InputValidation.java
@@ -0,0 +1,16 @@
+public class InputValidation {
+
+    public static boolean isValidAmount(double amount) {
+        return amount > 0 && amount < 1000;
+    }
+
+    public static boolean isValidCategory(String category) {
+        String[] validCategories = {"food", "travel", "bills", "entertainment", "other"};
+        for (String validCategory : validCategories) {
+            if (validCategory.equalsIgnoreCase(category)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
